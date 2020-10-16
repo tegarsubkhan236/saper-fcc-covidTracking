f8905bd7f6abc01704a8eb3636b03dbd7379e52f
tegarsubkhan236 <tegarsubkhan236@gmail.com>
2020-10-17 02:16:17 +0700

    setup css and pages

---
+++ src/routes/about.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+    <title>About COVID 19</title>
+</svelte:head>
+
+<div class="section header">
+    <div class="container">
+        <h1>About</h1>
+        <p>
+            The data for this site came from
+            <a href="https://covidtracking.com">The Covid Tracking Project</a>.
+            you can learn more about the by going to their site.
+        </p>
+    </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,16 @@
     import TableContainer from "../components/TableContainer.svelte";
 </script>
 
+<svelte:head>
+    <title>Covid 19 Tracker</title>
+</svelte:head>
+
+<div class="section header">
+    <div class="container">
+        <h1>Covid-19 The World</h1>
+    </div>
+</div>
+
 <CovidStat />
 <CovidChart />
 <TableContainer />
