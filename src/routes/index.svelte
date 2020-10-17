9f626e0965c55bc852c6315524ae61751ceeb7b3
tegarsubkhan236 <tegarsubkhan236@gmail.com>
2020-10-17 15:18:46 +0700

    got first covid us stats request done

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,21 @@
+<script context="module">
+    import requests from "../data/request.js";
+    export async function preload() {
+        try {
+            const usStats = await requests.usStats();
+
+            return { usStats };
+        } catch (e) {}
+    }
+</script>
+
 <script>
     import CovidChart from "../components/CovidChart.svelte";
     import CovidStat from "../components/CovidStat.svelte";
     import TableContainer from "../components/TableContainer.svelte";
+
+    export let usStats;
+    console.log(usStats, "usStats");
 </script>
 
 <svelte:head>
